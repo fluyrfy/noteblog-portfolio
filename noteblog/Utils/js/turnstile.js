23bd7d1b1d3025b99832bf765df4bd62d7620d47
Frank Liao <frank880703@gmail.com>
2023-11-19 14:06:45 +0800

    feat: turnstile theme

--- noteblog/Utils/js/turnstile.js
+++ noteblog/Utils/js/turnstile.js
@@ -1,6 +1,7 @@
 ﻿window.onloadTurnstileCallback = function () {
     turnstile.render('.cfturnstile', {
         sitekey: '0x4AAAAAAANJgZydX09IuVou',
+        theme: "light",
         callback: async function (token) {
             const result = await $.ajax({
                 url: '/api/verify/turnstile',
