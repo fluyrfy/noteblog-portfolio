225596be98f1736420fd843e509d6aee9af6c53e
Frank Liao <yufanliaocestlavie@gmail.com>
2023-12-03 22:28:38 +0800

    feat: dynamic note meta title desciption

--- noteblog/Controllers/StatsController.cs
+++ noteblog/Controllers/StatsController.cs
@@ -8,38 +8,38 @@ using noteblog.Utils;
 
 namespace noteblog.Controllers
 {
-  [RoutePrefix("api/stats")]
-  public class StatsController : ApiController
-  {
-    private readonly AccessStatsRepository _repository;
-
-    public StatsController()
+    [RoutePrefix("api/stats")]
+    public class StatsController : ApiController
     {
-      _repository = new AccessStatsRepository();
-    }
+        private readonly AccessStatsRepository _repository;
 
-    [HttpGet]
-    [Route("get")]
-    public object getStats(string startDate, string endDate)
-    {
-      var notes = _repository.getNotes(startDate, endDate);
-      var visits = _repository.getVisits(startDate, endDate);
-      var regions = _repository.getRegions(startDate, endDate);
-      return new
-      {
-        notes = notes,
-        visits = visits,
-        regions = regions
-      };
-    }
+        public StatsController()
+        {
+            _repository = new AccessStatsRepository();
+        }
 
-    [HttpPost]
-    [Route("save")]
-    public HttpResponseMessage saveStats([FromBody] dynamic value)
-    {
-      var pageName = value.pageName.Value;
-      string response = _repository.insert(pageName);
-      return Request.CreateResponse(HttpStatusCode.OK, response);
+        [HttpGet]
+        [Route("get")]
+        public object getStats(string startDate, string endDate)
+        {
+            var notes = _repository.getNotes(startDate, endDate);
+            var visits = _repository.getVisits(startDate, endDate);
+            var regions = _repository.getRegions(startDate, endDate);
+            return new
+            {
+                notes = notes,
+                visits = visits,
+                regions = regions
+            };
+        }
+
+        [HttpPost]
+        [Route("save")]
+        public HttpResponseMessage saveStats([FromBody] dynamic value)
+        {
+            var pageName = value.pageName.Value;
+            string response = _repository.insert(pageName);
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+        }
     }
-  }
 }
--- noteblog/Note.aspx.cs
+++ noteblog/Note.aspx.cs
@@ -8,68 +8,71 @@ using MySql.Data.MySqlClient;
 
 namespace noteblog
 {
-    public partial class Note : Page
+  public partial class Note : Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
     {
-        protected void Page_Load(object sender, EventArgs e)
+      // if (!IsPostBack)
+      // {
+      string noteIdString = Request.QueryString["id"];
+      if (string.IsNullOrEmpty(noteIdString))
+      {
+        Response.Redirect("Default.aspx");
+      }
+      else
+      {
+        if (int.TryParse(noteIdString, out int noteId))
         {
-            if (!IsPostBack)
+          try
+          {
+            using (MySqlConnection conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["Noteblog"].ConnectionString))
             {
-                string noteIdString = Request.QueryString["id"];
-                if (string.IsNullOrEmpty(noteIdString))
+              MySqlCommand cmd = new MySqlCommand();
+              cmd.Connection = conn;
+              StringBuilder sb = new StringBuilder();
+              sb.AppendLine("SELECT notes.*, users.name as user_name FROM notes INNER JOIN users ON notes.user_id = users.id WHERE 1 = 1");
+              sb.AppendLine("AND notes.id = @id");
+              cmd.Parameters.AddWithValue("@id", noteId);
+              cmd.CommandText = sb.ToString();
+              conn.Open();
+              using (MySqlDataReader reader = cmd.ExecuteReader())
+              {
+                if (reader.HasRows)
                 {
-                    Response.Redirect("Default.aspx");
+                  reader.Read();
+                  string title = reader["title"].ToString();
+                  string content = HttpUtility.HtmlDecode(reader["content"].ToString());
+                  string contentText = reader["content_text"].ToString();
+                  string keyword = reader["keyword"].ToString();
+                  litTitle.Text = title;
+                  litContent.Text = content;
+                  litAuthor.Text = reader["user_name"].ToString();
+                  Page.Title = title;
+                  Page.MetaDescription = contentText;
+                  Page.MetaKeywords = keyword;
+                  DateTime createdAt = (DateTime)reader["created_at"];
+                  litCreatedAt.Text = createdAt.ToString("MMMM dd, yyyy", new CultureInfo("en-US"));
                 }
                 else
                 {
-                    if (int.TryParse(noteIdString, out int noteId))
-                    {
-                        try
-                        {
-                            using (MySqlConnection conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["Noteblog"].ConnectionString))
-                            {
-                                MySqlCommand cmd = new MySqlCommand();
-                                cmd.Connection = conn;
-                                StringBuilder sb = new StringBuilder();
-                                sb.AppendLine("SELECT notes.*, users.name as user_name FROM notes INNER JOIN users ON notes.user_id = users.id WHERE 1 = 1");
-                                sb.AppendLine("AND notes.id = @id");
-                                cmd.Parameters.AddWithValue("@id", noteId);
-                                cmd.CommandText = sb.ToString();
-                                conn.Open();
-                                using (MySqlDataReader reader = cmd.ExecuteReader())
-                                {
-                                    if (reader.HasRows)
-                                    {
-                                        reader.Read();
-                                        litTitle.Text = reader["title"].ToString();
-                                        litContent.Text = HttpUtility.HtmlDecode(reader["content"].ToString());
-                                        litAuthor.Text = reader["user_name"].ToString();
-                                        DateTime createdAt = (DateTime)reader["created_at"];
-                                        litCreatedAt.Text = createdAt.ToString("MMMM dd, yyyy", new CultureInfo("en-US"));
-                                    }
-                                    else
-                                    {
-                                        Response.Redirect("~/Default.aspx");
-                                    }
-                                }
-
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            throw;
-                        }
-                        finally
-                        {
-                            new AccessStatsRepository().insert("Note");
-                        }
-
-                    }
-                    else
-                    {
-
-                    }
+                  Response.Redirect("~/Default.aspx");
                 }
+              }
+
             }
+          }
+          catch (Exception ex)
+          {
+            throw;
+          }
+          finally
+          {
+            new AccessStatsRepository().insert("Note");
+          }
+
         }
+      }
+      // }
     }
+  }
 }
