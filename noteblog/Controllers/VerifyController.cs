e90fa35d35be72bfac414c4ef0ef5a3f44bd4db7
Frank Liao <yufanliaocestlavie@gmail.com>
2024-01-09 23:09:41 +0800

    feat: looking up out rul

--- noteblog/Controllers/VerifyController.cs
+++ noteblog/Controllers/VerifyController.cs
@@ -3,46 +3,99 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace noteblog.Controllers
 {
-    [RoutePrefix("api/verify")]
-    public class VerifyController : ApiController
+  [RoutePrefix("api/verify")]
+  public class VerifyController : ApiController
+  {
+    [HttpPost]
+    [Route("turnstile")]
+    public async Task<HttpResponseMessage> turnstile()
     {
-        [HttpPost]
-        [Route("turnstile")]
-        public async Task<HttpResponseMessage> turnstile()
+      try
+      {
+        string SECRET_KEY = ConfigurationManager.AppSettings["TurnstileSecretKey"];
+        dynamic obj = await Request.Content.ReadAsAsync<JObject>();
+        var token = obj.token.ToString();
+        var formData = new FormUrlEncodedContent(new[]
         {
-            try
-            {
-                string SECRET_KEY = ConfigurationManager.AppSettings["TurnstileSecretKey"];
-                dynamic obj = await Request.Content.ReadAsAsync<JObject>();
-                var token = obj.token.ToString();
-                var formData = new FormUrlEncodedContent(new[]
-                {
                     new KeyValuePair<string, string>("secret", SECRET_KEY),
                     new KeyValuePair<string, string>("response", token)
                 });
-                var client = new HttpClient();
-                var response = await client.PostAsync("https://challenges.cloudflare.com/turnstile/v0/siteverify", formData);
-                string responseBody;
-                if (response.IsSuccessStatusCode)
-                {
-                    responseBody = await response.Content.ReadAsStringAsync();
-                }
-                else
-                {
-                    responseBody = "";
-                }
-                return Request.CreateResponse(HttpStatusCode.OK, responseBody);
-            }
-            catch (Exception ex)
-            {
-                return Request.CreateResponse(InternalServerError(ex));
-            }
+        var client = new HttpClient();
+        var response = await client.PostAsync("https://challenges.cloudflare.com/turnstile/v0/siteverify", formData);
+        string responseBody;
+        if (response.IsSuccessStatusCode)
+        {
+          responseBody = await response.Content.ReadAsStringAsync();
         }
+        else
+        {
+          responseBody = "";
+        }
+        return Request.CreateResponse(HttpStatusCode.OK, responseBody);
+      }
+      catch (Exception ex)
+      {
+        return Request.CreateResponse(InternalServerError(ex));
+      }
     }
+
+    [HttpPost]
+    [Route("url/safeBrowsing")]
+    public async Task<HttpResponseMessage> safeBrowsing()
+    {
+      try
+      {
+        string apiKey = ConfigurationManager.AppSettings["SafeBrowsingApiKey"];
+        string apiUrl = $"https://safebrowsing.googleapis.com/v4/threatMatches:find?key={apiKey}";
+        string requestBody = await Request.Content.ReadAsStringAsync();
+
+        using (HttpClient client = new HttpClient())
+        {
+          HttpResponseMessage response = await client.PostAsync(apiUrl, new StringContent(requestBody, Encoding.UTF8, "application/json"));
+               
+          string responseBody = await response.Content.ReadAsStringAsync();
+          return Request.CreateResponse(HttpStatusCode.OK, responseBody);
+        }
+      }
+      catch (Exception ex)
+      {
+        return Request.CreateResponse(InternalServerError(ex));
+      }
+    }
+
+    [HttpPost]
+    [Route("url/OPSWAT")]
+    public async Task<HttpResponseMessage> opswat()
+    {
+      try
+      {
+        string apiKey = ConfigurationManager.AppSettings["OPSWATApiKey"];
+        string requestBody = await Request.Content.ReadAsStringAsync();
+        dynamic requestData = JsonConvert.DeserializeObject(requestBody);
+        string url = requestData.url;
+        url = $"https://api.metadefender.com/v4/url/{url}";
+
+        using (HttpClient client = new HttpClient())
+        {
+          client.DefaultRequestHeaders.Add("apikey", apiKey);
+          HttpResponseMessage response = await client.GetAsync(url);
+          string responseBody = await response.Content.ReadAsStringAsync();
+          return Request.CreateResponse(HttpStatusCode.OK, responseBody);
+        }
+      }
+      catch (Exception ex)
+      {
+        return Request.CreateResponse(InternalServerError(ex));
+      }
+    }
+
+  }
 }
---
+++ noteblog/Utils/js/urlLookup.js
@@ -0,0 +1,91 @@
+﻿$("a").on("click", async function (event) {
+	let targetUrl = $(this).attr("href");
+	if (targetUrl && !isLocalUrl(targetUrl)) {
+		event.preventDefault();
+		targetUrl = encodeURIComponent(targetUrl);
+		const trSB = await checkForSafeBrowsing(targetUrl);
+		const trOPSWAT = await checkForOPSWAT(targetUrl);
+		if (trSB || trOPSWAT) {
+			const confirmed = confirm(
+				"This link may be harmful. Do you want to continue?"
+			);
+			if (confirmed) {
+				location.href = $(this).attr("href");
+			}
+		}
+	}
+});
+
+async function checkForSafeBrowsing(url) {
+	try {
+		const response = await fetch("/api/verify/url/safeBrowsing", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				client: {
+					clientId: "fl-note",
+					clientVersion: "1.0.0",
+				},
+				threatInfo: {
+					threatTypes: [
+						"MALWARE",
+						"SOCIAL_ENGINEERING",
+						"UNWANTED_SOFTWARE",
+						"POTENTIALLY_HARMFUL_APPLICATION",
+						"CSD_DOWNLOAD_WHITELIST",
+					],
+					platformTypes: ["ALL_PLATFORMS"],
+					threatEntryTypes: ["URL"],
+					threatEntries: [{ url }],
+				},
+			}),
+		});
+
+		const data = JSON.parse(await response.json());
+		return (
+			Object.getOwnPropertyNames(data).length > 0 && data.matches.length > 0
+		);
+	} catch (error) {
+		console.error("Error checking URL:", error);
+		return false;
+	}
+}
+
+async function checkForOPSWAT(url) {
+	try {
+		const response = await fetch("/api/verify/url/OPSWAT", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ url }),
+		});
+
+		const data = JSON.parse(await response.json());
+		const lookupResults = data.lookup_results;
+		if (lookupResults.detected_by > 0) {
+			const sources = lookupResults.sources;
+			for (const source of sources) {
+				if (source.status === 1) {
+					return true;
+				}
+			}
+		}
+		return false;
+	} catch (error) {
+		console.error("Error checking OPSWAT:", error);
+		throw error;
+	}
+}
+
+function isLocalUrl(url) {
+	if (!url.startsWith("http://") && !url.startsWith("https://")) {
+		return true;
+	}
+
+	const currentHostname = window.location.hostname;
+	const targetHostname = new URL(url).hostname;
+	return currentHostname === targetHostname;
+}
